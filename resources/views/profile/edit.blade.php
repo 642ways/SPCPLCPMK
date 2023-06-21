d0876f340fb92ae586b12ef2b1004308510bfcd1
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2023-06-21 19:42:51 +0700

    dashboard fix bug

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,7 +2,7 @@
 
     <div class="py-12">
         <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-16">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight ">
                 {{ __('Profile') }}
             </h2>
         </x-slot>
